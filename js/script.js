aa9da3af5399d277cb8b24b64ccfb3c5a1dc1f66
blastpay <pramothkumar.s@yantraservices.com>
2015-10-27 20:13:34 +0530

    sfff

--- js/script.js
+++ js/script.js
@@ -6,21 +6,18 @@ var speed = "500";
 //Document.Ready
 $(document).ready(function(){
   //Question handler
-$('li.q').on(action, function(){
-  //gets next element
-  //opens .a of selected question
-$(this).next().slideToggle(speed)
-    //selects all other answers and slides up any open answer
-    .siblings('li.a').slideUp();
-  
-  //Grab img from clicked question
-var img = $(this).children('img');
-  //Remove Rotate class from all images except the active
-  $('img').not(img).removeClass('rotate');
-  //toggle rotate class
-  img.toggleClass('rotate');
-
-});//End on click
+	$('li.q').on(action, function(){
+	  //gets next element
+	  //opens .a of selected question
+		$(this).next().slideToggle(speed)
+		//selects all other answers and slides up any open answer
+		.siblings('li.a').slideUp();
+		
+		$('li.q').css({"color": "#202020"}); 
+		$(this).css({"color": "#00A6D3"}); 
+	  
+	
+	});//End on click
 
 
 
